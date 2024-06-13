f0919f571bbc4b25fedced5ead4ebff009005281
Facundo Martín Mayordomo <85538820+Facu-May@users.noreply.github.com>
2024-06-13 00:03:28 -0300

    Actualización
    Seguir viendo cómo se implementa la eliminación de Heap. Cualquiera que tenga un apunte de progra2 que hable sobre heaps que avise jeje

--- src/implementaciones/Heap.java
+++ src/implementaciones/Heap.java
@@ -5,6 +5,7 @@ package implementaciones;
 public class Heap 
 {
     Nodo raiz; // nodo raíz del árbol
+    Nodo ultimoNodo; // nodo hoja más a la derecha
 
     // inicialización
     public Heap() 
@@ -23,10 +24,11 @@ public class Heap
         {
             // si el árbol está vacío, creamos un nuevo nodo y lo retornamos
             nodo = new Nodo(dato);
+            ultimoNodo = nodo; // el último nodo es el nodo recién creado
             return nodo;
         }
         // Si el nodo no es nulo, insertamos recursivamente en el subárbol izquierdo o derecho, dependiendo si el dato es mayor (subárbol izquierdo) o menor (subárbol derecho) que la raíz (nodo actual)
-        if(dato > raiz.info)
+        if(dato > nodo.info)
         {
             raiz.izq = insertarRec(raiz.izq, dato);
         }
@@ -52,8 +54,23 @@ public class Heap
         // retornamos el nodo actual
         return nodo;
     }
-    public void eliminar() // eli
+    public void eliminar() // eliminacion de elemento máximo (max-heap) o mínimo (min-heap)
     {
-        
+        if(raiz == null)
+        {
+            System.out.println("El heap está vacío");
+            return;
+        }
+        // reemplazo la raíz con el último nodo
+        raiz.info = ultimoNodo.info;
+
+        // eliminamos el último nodo
+        /*  Después seguir viendo esto porque la verdad que ya me mareé xD */
+
+    }
+
+    public Nodo getUltimoNodo()
+    {
+        return ultimoNodo;
     }
 }
